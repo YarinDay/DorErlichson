0a6e21d3b07b1bd8707542821edd05fc5aa0d2d7
Yarin <yarindayan11@gmail.com>
2023-09-28 22:41:10 +0300

    Changed prices

--- src/cmps/personal-price.jsx
+++ src/cmps/personal-price.jsx
@@ -7,19 +7,19 @@ export function PersonalPrice() {
             <div className='personal-price'>
                 <div className='personal-price-header' style={{ color: 'darkorange' }}>
                     <div className='personal-price-per-month'>
-                        <div>180₪ לשעה אימון זוגי</div>
+                        <div>220₪ לשעה אימון זוגי</div>
                     </div>
                 </div>
-                <div className='personal-price-content'>*170₪ לשעה בקניית כרטיסייה של 10 אימונים</div>
+                <div className='personal-price-content'>*210₪ לשעה בקניית כרטיסייה של 10 אימונים</div>
 
             </div>
             <div className='personal-price'>
                 <div className='personal-price-header' style={{ color: 'darkorange' }}>
                     <div className='personal-price-per-month'>
-                        <div>130₪ לשעה אימון אישי</div>
+                        <div>150₪ לשעה אימון אישי</div>
                     </div>
                 </div>
-                <div className='personal-price-content'>*120₪ לשעה בקניית כרטיסייה של 10 אימונים</div>
+                <div className='personal-price-content'>*145₪ לשעה בקניית כרטיסייה של 10 אימונים</div>
 
             </div>
         </div>
