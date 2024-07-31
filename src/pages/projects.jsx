5f08341c43e0eaca84b5dc5ab37a9bd630174a9c
Yarin Day <yarindayan11@gmail.com>
2024-07-31 23:15:40 +0300

    Changed to Image by antd

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -8,6 +8,7 @@ import process4 from "../assets/img/trainers/process4.png"
 import process5 from "../assets/img/trainers/process5.png"
 import process6 from "../assets/img/trainers/process6.png"
 import process7 from "../assets/img/trainers/process7.png"
+import { Image } from 'antd'
 
 export function Projects() {
     const processArr = [FirstImg, process4, process1, process6, process7, process5, process2, process3]
@@ -17,7 +18,11 @@ export function Projects() {
         <div className="app-home main-view main-layout project-section">
             <div className='headline-container process-container'>
                 {processArr.map((process, idx) => {
-                    return <img key={idx} src={process} alt='' />
+                    return  <Image
+                    key={idx}
+                    width={300}
+                    src={process}
+                  />
                 })}
             </div>
             <NavLink to='/' className={'back-btn text-decoration'}><span>חזור</span></NavLink>
