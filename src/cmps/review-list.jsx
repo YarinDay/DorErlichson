61ae57cc1a93ad416df8df69bb5a81485932dc4f
Yarin Day <yarindayan11@gmail.com>
2024-08-15 22:26:59 +0300

    Removed redundant function

--- src/cmps/review-list.jsx
+++ src/cmps/review-list.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { ReviewPreview } from './review-preview'
 import { Carousel } from 'react-responsive-carousel';
 
 export const ReviewList = () => {
